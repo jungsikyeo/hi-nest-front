edd84d19c92bb7838ff9822197159972290c92b6
jungsikyeo <mdotdom12@naver.com>
2021-02-15 00:48:03 +0900

    home page modify

--- src/pages/common/home.tsx
+++ src/pages/common/home.tsx
@@ -5,6 +5,7 @@ import circles from "../../images/circles.svg";
 import apple from "../../images/apple.svg";
 import google from "../../images/google.svg";
 import { Footer } from "./footer";
+import { Link } from "react-router-dom";
 
 export const Home = () => {
   return (
@@ -13,7 +14,10 @@ export const Home = () => {
         <title>Home | Podcast</title>
       </Helmet>
       <Header />
-      <main className="w-full flex flex-col items-center min-h-full max-h-full" style={{height: "450px"}}>
+      <main
+        className="w-full flex flex-col items-center max-h-full"
+        style={{ minHeight: "480px" }}
+      >
         <div
           className="w-full px-5 pt-14 text-4xl font-bold text-white"
           style={{
@@ -28,29 +32,47 @@ export const Home = () => {
             국내&nbsp;인기&nbsp;음악을 들어보세요
           </h1>
         </div>
-        <div className="w-full px-5 pt-8 flex text-base md:text-2xl text-white">
+        <div className="w-full px-5 pt-8 flex text-sm text-white">
           <span>
-            1개의 휴대폰에서 7일 동안 무료로 Spotify Premium 멤버십을
-            이용해보세요. 결제 정보는 필요하지 않습니다.
+            <p>
+              이 사이트는 노마드코더 강의 중 우버이츠 챌린지 과제 일환으로
+              만들어졌습니다.
+            </p>
+            <p>
+              최근 Spotify 사이트가 국내 버전으로 오픈되어, 제 우버이츠 챌린지
+              졸업 작품의 모티브가 되었습니다.
+            </p>
+            <br />
+            <p>
+              Host 권한으로 가입하면 Spotify API 를 이용하여 자신만의 자신만의
+              플레이리스트를 만들 수 있습니다.
+            </p>
+            <p>
+              Listener 권한으로 가입하면 Host가 만든 플레이리스트를 구독하고
+              음악을 청취할 수 있습니다.
+            </p>
           </span>
         </div>
         <div className="w-full px-5 py-8 flex flex-col items-center md:flex-row md:items-start">
           <span className="pb-3 md:pr-3">
-            <img src={google} className="h-10" alt="Google" />
+            <a href="https://play.google.com/store/apps/details?id=com.spotify.music">
+              <img src={google} className="h-10" alt="Google" />
+            </a>
           </span>
           <span className="">
-            <img src={apple} className="h-10" alt="Apple" />
+            <a href="https://apps.apple.com/app/spotify-music-and-podcasts/id324684580">
+              <img src={apple} className="h-10" alt="Apple" />
+            </a>
           </span>
         </div>
-        <div className="w-full px-5 mb-10 flex text-xs text-white tracking-tighter">
+        <div className="w-full px-5 mb-5 flex text-xs text-white tracking-tighter">
           <span>
-            약관이 적용됩니다. Premium을 이미 이용해 보셨거나 다른 기기로
-            이용하시려는 경우 이 혜택이 제공되지 않습니다. 7일 이후에도 계속
-            Spotify 서비스를 이용하시려면 Premium을 구독해야 합니다.
+            약관이 없습니다. 진짜 Spotify 서비스를 이용하시려면 위 링크를
+            클릭하여 앱을 설치하세요.
           </span>
         </div>
         <div
-          className="absolute top-0 flex flex-col items-center w-screen bg-gradient-to-b from-red-400 to-yellow-400"
+          className="absolute top-0 flex flex-col items-center w-screen h-screen bg-gradient-to-b from-red-400 to-yellow-400"
           style={{ zIndex: -1 }}
         >
           <div className="h-auto px-20" style={{ width: "900px" }}>
